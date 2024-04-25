3fd74915caa77c3f546866ec34493a6a5dd381ee
Romanyerom <Romanyerom@github.com>
2024-04-25 14:13:35 +0000

    update

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,5 @@
 export function taskFirst() {
-  const task = 'I prefer const when I can.';  // Changed 'var' to 'const'
+  const task = 'I prefer const when I can.'; // Changed var to const
   return task;
 }
 
@@ -8,8 +8,8 @@ export function getLast() {
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let';  // Changed 'var' to 'let'
-  combination += getLast();
+  let combination = 'But sometimes let'; // Changed var to let
+  combination += getLast(); // This is okay since let allows reassignment
 
   return combination;
 }
